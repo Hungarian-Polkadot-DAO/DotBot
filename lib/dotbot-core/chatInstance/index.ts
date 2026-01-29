2b38ab2f6da4166d7d65a4c523a8928d42f2465d
Peter Ott (Valeska) <ottpeter@tutanota.io>
2026-01-29 14:38:57 +0100

    adds index for ChatInstance

---
+++ lib/dotbot-core/chatInstance/index.ts
@@ -0,0 +1,9 @@
+/**
+ * ChatInstance
+ * 
+ * Main exports for chat instance management
+ */
+
+export { ChatInstance } from './chatInstance';
+export { ExecutionStateManager } from './executionState';
+export { ExecutionSessionManager } from './sessionManager';
